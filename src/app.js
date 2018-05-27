a7e9d332994089c6475869f7e4e7b3fec30da38f
Walter Lyons <walterlyons@walters-mini.home>
2018-05-27 09:25:34 -0700

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,19 +11,19 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(addExpense({ description: 'Poop party', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-// store.dispatch(setTextFilter('water'));
+// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+// store.dispatch(addExpense({ description: 'Gas bill' }));
+// store.dispatch(addExpense({ description: 'Poop party', createdAt: 1000}));
+// store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+// // store.dispatch(setTextFilter('water'));
 
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('bill'));
-// }, 3000)
+// // setTimeout(() => {
+// //   store.dispatch(setTextFilter('bill'));
+// // }, 3000)
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
