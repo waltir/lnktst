9762a3c019914253c2df89c819716466103ee8d0
Walter Lyons <walterlyons@walters-mini.home>
2018-05-28 08:05:45 -0700

    Setup production webpac build

--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@ git status - View the changes to your project code
 git add - Add files to staging area
 git commit - Creates a new commit with files from staging area
 git log - View recent commits
+
+npm run serve - Starts live server running on public folder
+yarn run dev-server - Compiles webpack
+yarn run build:prod - Builds for prod env
+yarn run build:dev - Builds for dev env
+yarn run test - Runs all of our JEST unit tests
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,52 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // WEBPACK NEEDS TO KNOW THE ENTRY POINT -> AND OUTPUT
 
-module.exports = {
-  entry: './src/app.js',
-  // entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css')
+
+  return {
+      entry: './src/app.js',
+      output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+      },
+      module: {
+        rules: [{
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        }, {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }]
+      },
+      plugins: [
+        CSSExtract
+      ],
+      devtool: isProduction ? 'source-map' : 'inline-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
+      }
+    };
 };
 
 // LOADER - LETS YOU CUSTOMIZE WEBPACK WHEN IT LOADS A GIVEN FILE
