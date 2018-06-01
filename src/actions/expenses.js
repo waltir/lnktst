e6ee4d728f178e671b2b603eb2879ab9aee88dad
Walter Lyons <walterlyons@walters-mini.home>
2018-06-01 06:27:36 -0700

    Get firebase expenses on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -15,9 +15,9 @@ export const startAddExpense = (expenseData = {}) => {
       amount = 0,
       createdAt = 0
     } = expenseData;
-    const expense = {description, note, amount, createdAt};
+    const expense = { description, note, amount, createdAt };
 
-    database.ref('expenses').push(expense).then((ref) => {
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
@@ -38,3 +38,25 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+      dispatch(setExpenses(expenses));
+
+    });
+  };
+};
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,5 +1,1 @@
-import DotEnv from 'dotenv';
-
-DotEnv.config({path: '.env.test'});
-
-// require('dotenv').config({path: '.env.test'});
+require('dotenv').config({ path: '.env.test' });
